76347423c858eabc9294b36c35e01e857e222c37
Yuanrui Zhang <yuanrz@uw.edu>
2017-11-14 13:58:45 -0800

    download it, bitch

--- exercise-1/scripts/BuildScatter.R
+++ exercise-1/scripts/BuildScatter.R
@@ -8,3 +8,31 @@
 #   - Title of the plot (set a default of "Title")
 #   - Label for the x axis (set a default of "X Title")
 #   - Label for the y axis (set a default of "Y Title")
+
+var.of.interest <- 'Petal.Length'
+vector.of.interest <- iris[, var.of.interest]
+
+wtf<-as.data.frame(vector.of.interest)
+
+BuildScatter <- function(data, xVar, yVar, colorVar, title="Title", xLab = "X Title", yLab = "Y Title") {
+  # Create plot using variables
+  p <- ggplot(data = data) + 
+    geom_point(mapping = aes(x = data[,xVar], y=data[,yVar], color = data[,colorVar])) + 
+    labs(title = title, x = xLab, y = yLab)
+  
+  # Return plot
+  return(p)
+}
+
+
+iris.df->(iris)
+
+
+BuildScatter(data = iris, 
+             xVar = 'Sepal.Length', 
+             yVar = 'Sepal.Width',
+             colorVar = 'Species', 
+             title = 'Iris Dataset', 
+             xLab = 'Sepal Length', 
+             yLab = 'Sepal Width'
+)
